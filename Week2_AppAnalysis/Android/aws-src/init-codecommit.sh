f39ad71936055e098cd919720b390832596ee193
natebachmeier <nate@bachmeier>
2021-03-01 23:04:38 -0500

    import into codecommit

--- Week2_AppAnalysis/Android/aws-src/init-codecommit.sh
+++ Week2_AppAnalysis/Android/aws-src/init-codecommit.sh
@@ -8,3 +8,10 @@ done
 for name in `cat random.subset`; do
   aws codeguru-reviewer --region us-east-2 associate-repository --repository "CodeCommit={Name=${name}}"
 done
+
+for name in `cat random.subset`; do
+  pushd $name/code
+  git push https://git-codecommit.us-east-2.amazonaws.com/v1/repos/$name --all
+  git push https://git-codecommit.us-east-2.amazonaws.com/v1/repos/$name --tags
+  popd
+done
